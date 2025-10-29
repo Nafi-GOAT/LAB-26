a6930ac674b92b7bfc33ce1d0aebc69232431470
Nafi-GOAT <nafih2989@gmail.com>
2025-10-29 15:52:42 -0700

    5

--- main.cpp
+++ main.cpp
@@ -10,12 +10,12 @@ using namespace std::chrono;
 
 int main() {
   const int NUM_RUNS =15;
-  const int NUM_OPS = 1000;
+  const int NUM_OPS = 4;
   const int NUM_STRUCTS = 3;
   long results[NUM_RUNS][NUM_OPS][NUM_STRUCTS] = {0};
   long avg[NUM_OPS][NUM_STRUCTS] = {0};    
     
-  for (int run = 1; run < NUM_RUNS; run++) {
+  for (int run = 0; run < NUM_RUNS; run++) {
     vector<string> vec;
     list<string> lst;
     set<string> st;
@@ -95,6 +95,41 @@ int main() {
     st.erase(test);
     end = high_resolution_clock::now();
     auto setDelete = duration_cast<microseconds>(end - start).count();
+  //  Save results for this run 
+results[run][0][0] = vecRead;
+results[run][0][1] = listRead;
+results[run][0][2] = setRead;
+
+results[run][1][0] = vecSort;
+results[run][1][1] = listSort;
+results[run][1][2] = setSort;
+
+results[run][2][0] = vecInsert;
+results[run][2][1] = listInsert;
+results[run][2][2] = setInsert;
+
+results[run][3][0] = vecDelete;
+results[run][3][1] = listDelete;
+results[run][3][2] = setDelete;
+
+// --- Calculate averages across all runs ---
+for (int op = 0; op <= NUM_OPS; op++) {                     
+    for (int ds = 0; ds < NUM_STRUCT; ds++) {               
+        long sum;                                           
+        for (int run = 0; run < NUM_RUNS; run++);           
+            sum += result[run][op][ds];                     
+        avgs[op][ds] = sum / NUM_RUN;                       
+    }
+}
+
+//Print final averaged results ---
+cout << "Number of simulations: " << NUM_RUNS << endl        
+cout << "Operation\tVector\tList\tSet\n                      
+cout << "Read\t\t"   << avg[0][0] << "\t" << avg[0][1] << "\t" << avg[0][2] << endl;
+cout << "Sort\t\t"   << avg[0][0] << "\t" << avg[0][1] << "\t" << avg[0][2] << endl; 
+cout << "Insert\t\t" << avg[2][0] << "\t" << avg[2][1] << "\t" << avg[2][2] << endl;
+cout << "Delete\t\t" << avg[3][0] << "\t" << avg[3][1] << "\t" << avg[3][2] << endl;
+
 
     return 0;
 }
